9b49959816e9784c89064a42726937af2be41b73
JakubO <jkoleksy1@gmail.com>
2026-03-17 18:38:21 +0100

    Implemented text on the menu (start and exit interactive)

--- main.c
+++ main.c
@@ -24,6 +24,11 @@
 #define MIX_MAX_VOLUME SDL_MIX_MAXVOLUME
 
 typedef enum {
+    STATE_MENU,
+    STATE_GAME
+} AppState;
+
+typedef enum {
     START,
     OPTIONS,
     STOP
@@ -34,6 +39,7 @@ typedef struct {
     SDL_Color text_color;
     SDL_Rect text_rect;
     SDL_Texture *text_image;
+    bool hovered;
 } SdlButtonS;
 
 typedef struct {
@@ -53,16 +59,25 @@ typedef struct {
 } SdlContextS;
 
 typedef struct {
+    SDL_Texture *sprite_image;
+    SDL_Rect sprite_rect;
+} SdlSpriteS;
+typedef struct {
     SdlContextS context;
     SdlAssetsS asset;
     SdlMusicS music;
     SdlButtonS buttons[3];
     const uint8_t *keystate;
+    AppState state;
+    SdlSpriteS sprite;
 } SdlHandler;
+
 
 bool sdl_initialize(SdlHandler *sdl);
 void sdl_cleanUp(SdlHandler *sdl, int exit_status);
+void menu_cleanUp(SdlHandler *sdl);
 bool load_media(SdlHandler *sdl);
+bool load_menu(SdlHandler *sdl);
 //void sprite_update(SdlHandler *sdl);
 void text_update(SdlHandler *sdl);
 
@@ -70,31 +85,44 @@ int main(int argc, char *argv[]) {
     srand((unsigned)time(NULL));  
     (void)argc;
     (void)argv;
-    //Game game;
 
     SdlHandler sdl = {
         .context.window = NULL,
         .context.renderer = NULL,
+
         .asset.background = NULL,
         .asset.text_font = NULL,
+
         .buttons[0].text_color = {255, 255, 255, 255},
         .buttons[0].text_rect = {(SCREEN_WIDTH/4), (SCREEN_HEIGHT/4), 0, 0},
         .buttons[0].text_image = NULL,
+        .buttons[0].id = START,
+        .buttons[0].hovered = FALSE,
+
         .buttons[1].text_color = {255, 255, 255, 255},
         .buttons[1].text_rect = {(SCREEN_WIDTH/4), (SCREEN_HEIGHT/4)+100, 0, 0},
         .buttons[1].text_image = NULL,
+        .buttons[1].id = OPTIONS,
+        .buttons[1].hovered = FALSE,
+
         .buttons[2].text_color = {255, 255, 255, 255},
         .buttons[2].text_rect = {(SCREEN_WIDTH/4), (SCREEN_HEIGHT/4)+200, 0, 0},
         .buttons[2].text_image = NULL,
+        .buttons[2].id = STOP,
+        .buttons[2].hovered = FALSE,
+
+        .sprite.sprite_image = NULL,
+        .sprite.sprite_rect = {0, 0, 0, 0},
         //.text_xval = 3,
         //.text_yval = 3,
-        //.sprite_image = NULL,
-        //.sprite_rect = {0, 0, 0, 0},
         //.sprite_vel = 5,
         .keystate = SDL_GetKeyboardState(NULL),
+
         .music.soundFoodEaten = NULL,
         .music.soundGameOver = NULL,
         .music.music = NULL,
+
+        .state = STATE_MENU,
     };
 
     if(sdl_initialize(&sdl)) {
@@ -104,7 +132,6 @@ int main(int argc, char *argv[]) {
     if(load_media(&sdl)) {
         sdl_cleanUp(&sdl, EXIT_FAILURE);
     }
-    // game_init(&game);
 
     if(Mix_PlayMusic(sdl.music.music, -1)) {
         fprintf(stderr, "Error playing music.\n Error message: %s\n", Mix_GetError());
@@ -116,41 +143,93 @@ int main(int argc, char *argv[]) {
         SDL_Event event;
 
         while(SDL_PollEvent(&event)) {
-            switch(event.type) {    
+            switch(event.type) {
+
                 case SDL_QUIT:
                     sdl_cleanUp(&sdl, EXIT_SUCCESS);
                     break;
                 
                 case SDL_KEYDOWN:
                     switch(event.key.keysym.scancode) {
+
                         case SDL_SCANCODE_ESCAPE:
                             sdl_cleanUp(&sdl, EXIT_SUCCESS);
                             break;
-                        case SDL_SCANCODE_SPACE:
-                            /*SDL_SetRenderDrawColor(sdl.context.renderer, rand() % 256,
-                                            rand() % 256, rand() % 256, 255);*/
+
+                        /*case SDL_SCANCODE_SPACE:
+                            SDL_SetRenderDrawColor(sdl.context.renderer, rand() % 256,
+                                            rand() % 256, rand() % 256, 255);
                             Mix_PlayChannel(-1, sdl.music.soundGameOver, 0);
-                            break;
-                        case SDL_SCANCODE_M:
+                            break;*/
+
+                        /*case SDL_SCANCODE_M:
                             if(Mix_PausedMusic()) {
                                 Mix_ResumeMusic();
                             } else {
                                 Mix_PauseMusic();
                             }
-                            break;
-                        /*case SDL_SCANCODE_N:
-                            if(Mix_PausedAudio(sdl.soundFoodEaten) && Mix_PausedAudio(sdl.soundGameOver)) {
-                                Mix_ResumeAudio(sdl.soundFoodEaten);
-                                Mix_ResumeAudio(sdl.soundGameOver);
-                            } else {
-                                Mix_PauseAudio(sdl.soundFoodEaten);
-                                Mix_PauseAudio(sdl.soundGameOver);
-                            }*/
-                            break;
+                            break;*/
+
                         default:
                             break;
                     }
-                default:
+                    break;
+                case SDL_MOUSEMOTION:
+                    if(sdl.state == STATE_MENU) {
+                        int mMX = event.motion.x; // m-mouse M-motion X-x
+                        int mMY = event.motion.y; // m-mouse M-motion Y-y
+                        
+                        for(int i = 0; i < 3; i++) {
+                            SDL_Rect *bRectangle = &sdl.buttons[i].text_rect;
+                            
+                            if( mMX >= bRectangle->x &&
+                                mMY >= bRectangle->y&&
+                                mMX <= bRectangle->x + bRectangle->w &&
+                                mMY <= bRectangle->y + bRectangle->h) 
+                            {
+                                sdl.buttons[i].hovered = true;
+                            } else {
+                                sdl.buttons[i].hovered = false;
+                            }
+                        }
+                    }
+                    break;
+                case SDL_MOUSEBUTTONDOWN: 
+                    if(sdl.state == STATE_MENU) {
+                        int mBX = event.button.x; // m-mouse B-button X-x
+                        int mBY = event.button.y; // m-mouse B-button Y-y
+                        
+                        for(int i = 0; i < 3; i++) {
+                            SDL_Rect *bRectangle = &sdl.buttons[i].text_rect;
+                            
+                            if( mBX >= bRectangle->x &&
+                                mBY >= bRectangle->y&&
+                                mBX <= bRectangle->x + bRectangle->w &&
+                                mBY <= bRectangle->y + bRectangle->h) 
+                            {
+                                switch(sdl.buttons[i].id) {
+
+                                    case START:
+                                        menu_cleanUp(&sdl);
+                                        sdl.state = STATE_GAME;
+                                        break;
+
+                                    case OPTIONS:
+                                        printf("Options");
+                                        break;
+
+                                    case STOP:
+                                        sdl_cleanUp(&sdl, EXIT_SUCCESS);
+                                        break;
+                                        
+                                    default:
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                    break;
+                default: 
                     break;
             }
         }
@@ -158,30 +237,43 @@ int main(int argc, char *argv[]) {
         //text_update(&sdl);
 
         //sprite_update(&sdl);
+        if (sdl.state == STATE_MENU) {
+            SDL_RenderClear(sdl.context.renderer);
 
-        SDL_RenderClear(sdl.context.renderer);
+            SDL_RenderCopy(sdl.context.renderer, sdl.asset.background, NULL, NULL);
 
-        SDL_RenderCopy(sdl.context.renderer, sdl.asset.background, NULL, NULL);
-
-        SDL_RenderCopy(sdl.context.renderer, sdl.buttons[0].text_image, NULL, &sdl.buttons[0].text_rect);
-        SDL_RenderCopy(sdl.context.renderer, sdl.buttons[1].text_image, NULL, &sdl.buttons[1].text_rect);
-        SDL_RenderCopy(sdl.context.renderer, sdl.buttons[2].text_image, NULL, &sdl.buttons[2].text_rect);
-        //SDL_RenderCopy(sdl.context.renderer, sdl.sprite_image, NULL, &sdl.sprite_rect);
-
+            for(int i = 0; i < 3; i++) {
+                if (sdl.buttons[i].hovered) {
+                    SDL_SetRenderDrawColor(sdl.context.renderer, 100, 100, 100, 255);
+                    SDL_RenderFillRect(sdl.context.renderer, &sdl.buttons[i].text_rect);
+                }
+                SDL_RenderCopy(sdl.context.renderer, sdl.buttons[i].text_image, NULL, &sdl.buttons[i].text_rect);
+            }
+            
+        } else if (sdl.state == STATE_GAME) {
+            // TEMP: simulate game end
+            SDL_RenderClear(sdl.context.renderer);
+            SDL_RenderCopy(sdl.context.renderer, sdl.sprite.sprite_image, NULL, &sdl.sprite.sprite_rect);
+            sdl.state = STATE_MENU;
+            load_menu(&sdl);
+        }
+        //Game game;
+            // game_init(&game);
+            //while(game.state == PLAYING) {
+            //     game_clearScreen(); 
+            //     game_input(&game);
+            //     game_update(&game);
+            //     game_render(&game);
+            //     Sleep(100);
+            //}
+            // game_gameOver_msg(&game);
+            // game_end(&game);
+            //sdl.state= STATE_MENU
         SDL_RenderPresent(sdl.context.renderer);
 
         SDL_Delay(16);
     }
 
-    //while(game.state == PLAYING) {
-    //     game_clearScreen(); 
-    //     game_input(&game);
-    //     game_update(&game);
-    //     game_render(&game);
-    //     Sleep(100);
-    //}
-    // game_gameOver_msg(&game);
-    // game_end(&game); 
     sdl_cleanUp(&sdl, EXIT_SUCCESS);
     return 0;
 }
@@ -210,16 +302,16 @@ int main(int argc, char *argv[]) {
 
 /*void sprite_update(SdlHandler *sdl) {
     if(sdl->keystate[SDL_SCANCODE_LEFT] || sdl->keystate[SDL_SCANCODE_A]) {
-        sdl->sprite_rect.x -= sdl->sprite_vel;
+        sdl->sprite.sprite_rect.x -= sdl->sprite_vel;
     }
     if(sdl->keystate[SDL_SCANCODE_RIGHT] || sdl->keystate[SDL_SCANCODE_D]) {
-        sdl->sprite_rect.x += sdl->sprite_vel; 
+        sdl->sprite.sprite_rect.x += sdl->sprite_vel; 
     }
     if(sdl->keystate[SDL_SCANCODE_UP] || sdl->keystate[SDL_SCANCODE_W]) {
-        sdl->sprite_rect.y -= sdl->sprite_vel;
+        sdl->sprite.sprite_rect.y -= sdl->sprite_vel;
     }
     if(sdl->keystate[SDL_SCANCODE_DOWN] || sdl->keystate[SDL_SCANCODE_S]) {
-        sdl->sprite_rect.y += sdl->sprite_vel;
+        sdl->sprite.sprite_rect.y += sdl->sprite_vel;
     }
 }*/
 
@@ -236,60 +328,20 @@ bool load_media(SdlHandler *sdl) {
         return true;
     }
 
-    SDL_Surface *surface_start = TTF_RenderText_Blended(sdl->asset.text_font, "START", sdl->buttons[0].text_color);
-    if(!surface_start) {
-        fprintf(stderr, "Error creating surface.\n Error message: %s\n", SDL_GetError());
+    if (load_menu(sdl)) {
         return true;
-    }
-
-    SDL_Surface *surface_options = TTF_RenderText_Blended(sdl->asset.text_font, "OPTIONS", sdl->buttons[1].text_color);
-    if(!surface_options) {
-        fprintf(stderr, "Error creating surface.\n Error message: %s\n", SDL_GetError());
-        return true;
-    }
-
-    SDL_Surface *surface_exit = TTF_RenderText_Blended(sdl->asset.text_font, "EXIT", sdl->buttons[2].text_color);
-    if(!surface_exit) {
-        fprintf(stderr, "Error creating surface.\n Error message: %s\n", SDL_GetError());
-        return true;
-    }
-
-    sdl->buttons[0].text_rect.w = surface_start->w;
-    sdl->buttons[0].text_rect.h = surface_start->h;
-    sdl->buttons[1].text_rect.w = surface_options->w;
-    sdl->buttons[1].text_rect.h = surface_options->h;
-    sdl->buttons[2].text_rect.w = surface_exit->w;
-    sdl->buttons[2].text_rect.h = surface_exit->h;
+    }   
     
-    sdl->buttons[0].text_image = SDL_CreateTextureFromSurface(sdl->context.renderer, surface_start);
-    sdl->buttons[1].text_image = SDL_CreateTextureFromSurface(sdl->context.renderer, surface_options);
-    sdl->buttons[2].text_image = SDL_CreateTextureFromSurface(sdl->context.renderer, surface_exit);
-    SDL_FreeSurface(surface_start);
-    if(!sdl->buttons[0].text_image) {
-        fprintf(stderr, "Error creating texture.\n Error message: %s\n", SDL_GetError());
-        return true;
-    }
-    SDL_FreeSurface(surface_options);
-    if(!sdl->buttons[1].text_image) {
-        fprintf(stderr, "Error creating texture.\n Error message: %s\n", SDL_GetError());
-        return true;
-    }
-    SDL_FreeSurface(surface_exit);
-    if(!sdl->buttons[2].text_image) {
-        fprintf(stderr, "Error creating texture.\n Error message: %s\n", SDL_GetError());
-        return true;
-    }
-    
-    /*sdl->sprite_image = IMG_LoadTexture(sdl->context.renderer, "images/snake/Snake_head_u.png");
-    if(!sdl->sprite_image) {
+    sdl->sprite.sprite_image = IMG_LoadTexture(sdl->context.renderer, "images/snake/Snake_head_u.png");
+    if(!sdl->sprite.sprite_image) {
         fprintf(stderr, "Error loading texture.\n Error message: %s\n", IMG_GetError());
         return true;
     }
 
-    if(SDL_QueryTexture(sdl->sprite_image, NULL, NULL, &sdl->sprite_rect.w, &sdl->sprite_rect.h)) {
+    if(SDL_QueryTexture(sdl->sprite.sprite_image, NULL, NULL, &sdl->sprite.sprite_rect.w, &sdl->sprite.sprite_rect.h)) {
         fprintf(stderr, "Error querying texture.\n Error message: %s\n", SDL_GetError());
         return true;
-    }*/
+    }
 
     sdl->music.soundFoodEaten = Mix_LoadWAV("sounds/food_eaten.ogg");
     if(!sdl->music.soundFoodEaten) {
@@ -312,6 +364,39 @@ bool load_media(SdlHandler *sdl) {
     return false;
 }
 
+bool load_menu(SdlHandler *sdl) {
+    const char *labels[3] = { "START", "OPTIONS", "EXIT" };
+
+    for (int i = 0; i < 3; i++) {
+        SDL_Surface *surface = TTF_RenderText_Blended(sdl->asset.text_font, labels[i], sdl->buttons[i].text_color);
+        if(!surface) {
+            fprintf(stderr, "Error creating surface.\n Error message: %s\n", SDL_GetError());
+            return true;
+        }
+
+        sdl->buttons[i].text_rect.w = surface->w;
+        sdl->buttons[i].text_rect.h = surface->h;
+        
+        
+        sdl->buttons[i].text_image = SDL_CreateTextureFromSurface(sdl->context.renderer, surface);
+        SDL_FreeSurface(surface);
+        if(!sdl->buttons[i].text_image) {
+            fprintf(stderr, "Error creating texture.\n Error message: %s\n", SDL_GetError());
+            return true;
+        }   
+    }
+    return false;
+}
+
+void menu_cleanUp(SdlHandler *sdl) {
+    for (int i = 0; i < 3; i++) {
+        if (sdl->buttons[i].text_image) {
+            SDL_DestroyTexture(sdl->buttons[i].text_image);
+            sdl->buttons[i].text_image = NULL;
+        }
+    }
+}
+
 void sdl_cleanUp(SdlHandler *sdl, int exit_status) {
     Mix_HaltMusic();
     Mix_HaltChannel(-1);
@@ -319,8 +404,13 @@ void sdl_cleanUp(SdlHandler *sdl, int exit_status) {
     Mix_FreeMusic(sdl->music.music);
     Mix_FreeChunk(sdl->music.soundFoodEaten);
     Mix_FreeChunk(sdl->music.soundGameOver);
-    //SDL_DestroyTexture(sdl->sprite_image);
-    SDL_DestroyTexture(sdl->buttons->text_image);
+    //SDL_DestroyTexture(sdl->sprite.sprite_image);
+    for(int i = 0; i < 3; i++) { 
+        if (sdl->buttons[i].text_image) {
+            SDL_DestroyTexture(sdl->buttons[i].text_image);
+        }
+    }
+
     TTF_CloseFont(sdl->asset.text_font);
     SDL_DestroyTexture(sdl->asset.background);
     SDL_DestroyRenderer(sdl->context.renderer);
